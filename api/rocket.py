149acf5f07a75d3867f132bd9a6b5733883dab94
tanayp327 <tanaypatel327@gmail.com>
2023-05-31 16:52:39 -0700

    wowza

---
+++ api/rocket.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, Blueprint
+from flask_restful import Resource, Api
+
+rocket_api = Blueprint('rocket_api', __name__, 
+                            url_prefix='/api/rocket')
+api = Api(rocket_api)
+
+# Constants
+GRAVITY = 9.8  # Acceleration due to gravity
+
+# Create a resource for handling game requests
+class GameResource(Resource):
+    def post(self):
+        # Get the player's input from the request
+        data = request.get_json()
+
+        # Process the player's input and calculate the rocket's trajectory
+        thrust = data['thrust']
+        drag = data['drag']
+        time = data['time']
+
+        velocity = thrust - drag - GRAVITY * time
+        altitude = 0.5 * (thrust - drag) * time**2 - GRAVITY * time**2
+
+        # Return the result as a JSON response
+        return jsonify(velocity=velocity, altitude=altitude)
+
+# Add the resource to the API
+api.add_resource(GameResource, '/game')
--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from api.user import user_api # Blueprint import api definition
 from api.exercise import exercise_api
 from api.film import film_api
 from api.leaderboard import leaderboard_api
+from api.rocket import rocket_api
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -26,6 +27,7 @@ app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(exercise_api)
 app.register_blueprint(film_api)
 app.register_blueprint(leaderboard_api)
+app.register_blueprint(rocket_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
